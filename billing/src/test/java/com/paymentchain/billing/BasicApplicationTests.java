be52cbbb053f41701ac9642787f5f3fc6be4764d
Cristian Machuca <cmachuca32@gmail.com>
2023-03-12 10:52:31 -0700

    Testing Pipeline Jenkins.

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops 01";
+ 	    String meessage= "default message cambio test devops";
  	    Assert.assertNotNull(meessage);
  }
  
